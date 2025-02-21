4fc02f5c23086241f1a8df7f91b501f6fcec710c
Luis Daniel Suarez Escamilla <230040@utxicotepec.edu.mx>
2025-02-21 13:45:16 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'MANUAL_EJERCICIOS_5A_230629';
+}
