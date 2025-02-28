a820f328dc3e3aab5721d981f6325355ddf331cb
ppyo1234 <230629@utxicotepec.edu.mx>
2025-02-28 01:33:34 -0600

    Practica07
    Se agrega ,mejor estructura para la app web

--- ManualEjercicios_5A_230629/src/app/app.component.ts
+++ ManualEjercicios_5A_230629/src/app/app.component.ts
@@ -4,14 +4,20 @@ import { MatButtonModule } from '@angular/material/button'
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { AppSidebarComponent } from './components/sidebar/app-sidebar.component';
 import { AppPageContentComponent } from './components/page-content/app-page-content.component';
+import { CommonModule } from '@angular/common'; // Importar CommonModule
+import { FormsModule } from '@angular/forms'; // Para usar [(ngModel)]
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule, NavbarComponent,AppSidebarComponent, AppPageContentComponent],
+  imports: [RouterOutlet, MatButtonModule, NavbarComponent,AppSidebarComponent, AppPageContentComponent,CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ManualEjercicios_5A_230629';
-  
+  isSidebarOpen = true;
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 }
